de657690f52d2c500d2f39a01603bc5fba1557ee
Will Beasley <wibeasley@hotmail.com>
2018-09-12 09:13:41 -0500

    include rhub in procedure

--- documentation-for-developers/refresh.R
+++ documentation-for-developers/refresh.R
@@ -19,5 +19,6 @@ test_results_not_checked <- testthat::test_dir("./tests/manual/")
 
 # devtools::check(force_suggests = FALSE)
 # devtools::build_win(version="R-devel") #CRAN submission policies encourage the development version
+# devtools::check_rhub(email="wibeasley@hotmail.com")
 # devtools::revdep_check(pkg="codified", recursive=TRUE)
 # devtools::release(check=FALSE) #Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
